81bcb0661040a4b9d9667bb110c00e303c046b75
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-01-17 11:06:47 +0400

    update divide and conquer

--- 7kyu/divide_and_conquer.js
+++ 7kyu/divide_and_conquer.js
@@ -1,24 +1,13 @@
 function divCon(x){
-    var num = [];
-    var str= [];
-  
-    const tempNum = x.filter(e => typeof e == 'number');
-    const tempStr = x.filter(e => typeof e == 'string');
-  
-    console.log('tempNum', tempNum);
-    console.log('tempStr', tempStr);
-  
-    const sumNum = tempNum.reduce((a, b) => a + b, 0);
-    console.log('sumNum', sumNum);
-  
-  
-    const strToNum = tempStr.map(Number);
-  
-    console.log('strToNum', strToNum);
-  
-    const sumInt = strToNum.reduce((a, b)=> a + b, 0);
-  
-    console.log('sumInt', sumInt);
-  
-    return sumNum - sumInt;
-  }
+  const tempNum = x.filter(e => typeof e == 'number');
+  const tempStr = x.filter(e => typeof e == 'string');
+
+  const sumNum = tempNum.reduce((a, b) => a + b, 0);
+
+
+  const strToNum = tempStr.map(Number);
+
+  const sumInt = strToNum.reduce((a, b)=> a + b, 0);
+
+  return sumNum - sumInt;
+}
