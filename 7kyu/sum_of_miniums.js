264db27290c3ec2baefec10486662d889fdc53d2
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-02-07 11:16:18 +0400

    new kata 07.02.20

---
+++ 7kyu/sum_of_miniums.js
@@ -0,0 +1,16 @@
+function sumOfMinimums(arr) {
+  const temp = [];
+  const arrSort = [];
+
+  arr.forEach(function(e) {
+    temp.push(e.sort());
+  });
+
+  for (var i = 0; i < temp.length; i++) {
+    arrSort.push(temp[i][0]);
+  }
+
+  const total = arrSort.reduce((a, b) => a + b, 0);
+
+  return total;
+}
