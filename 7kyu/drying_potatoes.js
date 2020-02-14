966a241a3fa8343a7acdcba17fb1207ed43e78ef
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-02-14 14:03:20 +0400

    new katas 14.02.20

---
+++ 7kyu/drying_potatoes.js
@@ -0,0 +1,4 @@
+function potatoes(p0, w0, p1) {
+  // your code
+  return Math.floor((w0 * (100 - p0)) / (100 - p1));
+}
---
+++ 7kyu/no_oddities_here.js
@@ -0,0 +1,6 @@
+function noOdds(values) {
+  // Return all non-odd values
+  const temp = values.filter(n => !(n % 2));
+
+  return temp;
+}
