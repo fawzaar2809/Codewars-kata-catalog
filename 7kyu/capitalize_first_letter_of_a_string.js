861855b253a3b9c5e2f04aca663d9fa592df4599
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-03-05 11:26:41 +0400

    new katas 05.03.20

--- 7kyu/capitalize_first_letter_of_a_string.js
+++ 7kyu/capitalize_first_letter_of_a_string.js
@@ -3,8 +3,42 @@
  *  "Description" and click on the pseudo-link to reveal the answer
  */
 
-String.prototype.capitalize = function() {
+/* String.prototype.capitalize = function() {
   return this.replace(/^\D/, firstLetter =>
     String.fromCharCode(firstLetter.charCodeAt(0) - 32)
+  );
+}; */
+
+String.prototype.capitalize = function() {
+  let dict = {
+    a: "A",
+    b: "B",
+    c: "C",
+    d: "D",
+    e: "E",
+    f: "F",
+    g: "G",
+    h: "H",
+    i: "I",
+    j: "J",
+    k: "K",
+    l: "L",
+    m: "M",
+    n: "N",
+    o: "O",
+    p: "P",
+    q: "Q",
+    r: "R",
+    s: "S",
+    t: "T",
+    u: "U",
+    v: "V",
+    w: "W",
+    x: "X",
+    y: "Y",
+    z: "Z"
+  };
+  return (
+    this.toString()[0].replace(/[a-z]/, v => dict[v]) + this.toString().slice(1)
   );
 };
