ff6173ddba0184ac1983c18764a0745ca8bb492b
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-03-06 08:59:47 +0400

    new katas 06.03.2020

---
+++ 8kyu/shifty_closures.js
@@ -0,0 +1,10 @@
+let name = "Abe";
+
+const greet_abe = function() {
+  return "Hello, " + name + "!";
+};
+
+const greet_ben = function() {
+  let name = "Ben";
+  return "Hello, " + name + "!";
+};
