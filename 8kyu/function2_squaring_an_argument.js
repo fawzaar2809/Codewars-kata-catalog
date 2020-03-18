5195e9e0b9583be11c9e53549bd3c215ff206ed9
Fawzaar Woozeer <fawzaar_woozeer@hotmail.com>
2020-03-18 10:46:02 +0400

    new katas 18.03.2020

---
+++ 8kyu/function2_squaring_an_argument.js
@@ -0,0 +1,5 @@
+// Write the "square"-function here
+
+function square(x) {
+  return x * x;
+}
---
+++ 8kyu/string_cleaning.js
@@ -0,0 +1,5 @@
+function stringClean(s) {
+  // Function will return the cleaned string
+
+  return s.replace(/\d/g, "");
+}
